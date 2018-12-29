7b077d44df98d1483ea12a0c176480428b014be2
Dan Girshovich <dan@rigetti.com>
2018-12-29 07:11:17 -0800

    add initial ability to evaluate strings of Julia code from Emacs via jl_eval_string

--- emacs-julia-tests.el
+++ emacs-julia-tests.el
@@ -1,7 +1,8 @@
-(add-to-list 'load-path (expand-file-name "."))
-
 (require 'julia)
 (require 'ert)
 
-(ert-deftest julia-test ()
-  (should (= (julia-tester) (sqrt 2))))
+(ert-deftest julia-smoke-test ()
+  (should (= (julia-sqrt2) (sqrt 2))))
+
+(ert-deftest julia-eval-test ()
+  (should (= (julia-eval "sqrt(7)") (sqrt 7))))
--- emacs-julia.c
+++ emacs-julia.c
@@ -1,13 +1,56 @@
 #include "emacs-module.h"
 #include <stdio.h>
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 #include <julia.h>
 #include "emacs-module-helpers.h"
 
 int plugin_is_GPL_compatible;
 
-static emacs_value Fjulia_tester (emacs_env *env)
+static char*
+retrieve_string(emacs_env *env, emacs_value str, ptrdiff_t *size)
 {
+	*size = 0;
 
+	env->copy_string_contents(env, str, NULL, size);
+	char *p = malloc(*size);
+	if (p == NULL) {
+		*size = 0;
+		return NULL;
+	}
+	env->copy_string_contents(env, str, p, size);
+
+	return p;
+}
+
+static emacs_value
+Fjulia_eval (emacs_env *env, ptrdiff_t nargs, emacs_value args[], void *data)
+{
+  ptrdiff_t size;
+  char *str_arg = retrieve_string(env, args[0], &size);
+  jl_value_t *ret = jl_eval_string(str_arg);
+
+  double ret_unboxed = 0.0;
+
+  if (jl_typeis(ret, jl_float64_type)) {
+    ret_unboxed = jl_unbox_float64(ret);
+  }
+  // FIXME: Handle non-float return types here!
+  else {
+    printf("ERROR: unexpected return type when evaluating buffer.\n");
+  }
+
+  return env->make_float(env, ret_unboxed);
+  /* return env->make_string(env, str_arg); */
+}
+
+static emacs_value Fjulia_sqrt2 (emacs_env *env)
+{
   jl_value_t *ret = jl_eval_string("sqrt(2.0)");
 
   double ret_unboxed = 0.0;
@@ -20,7 +63,7 @@ static emacs_value Fjulia_tester (emacs_env *env)
     printf("ERROR: unexpected return type from sqrt(::Float64)\n");
   }
 
-  return env->make_float (env, ret_unboxed);
+  return env->make_float(env, ret_unboxed);
 }
 
 int emacs_module_init(struct emacs_runtime *ert)
@@ -29,9 +72,8 @@ int emacs_module_init(struct emacs_runtime *ert)
 
   jl_init();
 
-  DEFUN("julia-tester", Fjulia_tester, 0, 0,
-        "testitest\n",
-        0);
+  DEFUN("julia-sqrt2", Fjulia_sqrt2, 0, 0, "testitest\n", 0);
+  DEFUN("julia-eval", Fjulia_eval, 1, 1, "eval in julia\n", 0);
   provide(env, "julia-tester");
 
   return 0;
