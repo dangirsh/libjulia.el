43bd66a6f63e004a3681a63369d760a04fac4956
Dan Girshovich <dan@rigetti.com>
2018-12-31 09:47:33 -0800

    some macros to help generate ffi bindings to libjulia

--- julia-ffi-test.el
+++ julia-ffi-test.el
@@ -1,43 +1,81 @@
 (require 'ert)
 
-(add-to-list 'load-path (concat default-directory "/emacs-ffi"))
+(add-to-list 'load-path (concat default-directory "emacs-ffi"))
 
 (require 'ffi)
+(require 's)
 
-(define-ffi-library julia-core.so "julia-core.so")
+(define-ffi-library libjulia.so "libjulia.so")
 
-(define-ffi-function jl-eval-string "jl_eval_string" :pointer (:pointer) julia-core.so)
+(defun elisp-to-c-name (elisp-name)
+  (format "%s" (s-replace "-" "_" elisp-name)))
 
-(define-ffi-function jl-unbox-float64 "jl_unbox_float64" :float (:pointer) julia-core.so)
+(defmacro libjulia-bind (name arg-types return-type)
+  "Wrapper around define-ffi-function from the ffi library."
+  (message (format "Generating Julia binding for %s." name))
+  `(define-ffi-function
+     ,name
+     ,(elisp-to-c-name (symbol-name name))
+     ,return-type
+     ,arg-types
+     libjulia.so))
 
-(define-ffi-function jl-unbox-int64 "jl_unbox_int64" :int (:pointer) julia-core.so)
+(setq julia-fii-type-map
+      '(("int8" . :int)
+        ("uint8" . :int)
+        ("int16" . :int)
+        ("uint16" . :int)
+        ("int32" . :int)
+        ("uint32" . :int)
+        ("int64" . :int)
+        ("uint64" . :int)
+        ("int64" . :int)
+        ("float32" . :float)
+        ("float64" . :double)))
 
-(define-ffi-function jl-string-ptr "jl_string_ptr" :pointer (:pointer) julia-core.so)
 
-(define-ffi-function jl-typeof "jl_typeof_str" :pointer (:pointer) julia-core.so)
+(defun julia-ffi-gen-unboxers ()
+  (dolist (type-entry julia-fii-type-map)
+    (eval `(libjulia-bind
+            ,(intern  (format "jl-unbox-%s" (car type-entry)))
+            (:pointer)
+            ,(cdr type-entry)))))
+
+
+(defun julia-ffi-init ()
+  (julia-ffi-gen-unboxers))
+
+
+(libjulia-bind jl-string-ptr (:pointer) :pointer)
+(libjulia-bind jl-typeof-str (:pointer) :pointer)
+(libjulia-bind jl-eval-string (:pointer) :pointer)
+
 
 (defun jl-get-type (julia-expr-str)
-  (ffi-get-c-string (jl-typeof (jl-eval-string (ffi-make-c-string julia-expr-str)))))
+  (ffi-get-c-string (jl-typeof-str (jl-eval-string (ffi-make-c-string "3")))))
 
 (ert-deftest test-julia-ffi-get-type ()
   (should (equal (jl-get-type "3") "Int64")))
 
 
+(defun julia-unbox (type-str ptr)
+  (pcase type-str
+    ("Int64"
+     (jl-unbox-int64 ptr))
+    ("Float64"
+     (jl-unbox-float64 ptr))
+    ("String"
+     (ffi-get-c-string (jl-string-ptr ptr)))
+    (_
+     (error (format "Unboxing of Julia type %s unimplemented" type-str)))))
+
+
 (defun julia-eval (julia-expr-str)
   (let* ((ptr (jl-eval-string (ffi-make-c-string julia-expr-str)))
-         (type-str (ffi-get-c-string (jl-typeof ptr))))
-    (message (format "%s\n" type-str))
-    (cond
-     ((equal type-str "Int64")
-      (jl-unbox-int64 ptr))
-     ((equal type-str "Float64")
-      (jl-unbox-float64 ptr))
-     ((equal type-str "String")
-      (ffi-get-c-string (jl-string-ptr ptr)))
-     (t
-      (error (format "Unboxing of Julia type %s unimplemented" type-str))))))
+         (type-str (ffi-get-c-string (jl-typeof-str ptr))))
+    (julia-unbox type-str ptr)))
 
 (ert-deftest test-julia-eval ()
-  (should (eq (julia-eval "3") 3))
-  (should (eq (julia-eval "3.14") 3.14))
-  (should (equal (julia-eval "\"3.14\"") "3.14")))
+  (should (equal (julia-eval "3") 3))
+  (should (equal (julia-eval "3.14") 3.14))
+  (should (equal (julia-eval "\"Hallo\"") "Hallo")))
