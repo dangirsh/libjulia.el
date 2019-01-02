37b2aed3f03a6c2a0cefab601fe023489e207ed6
Dan Girshovich <dan@rigetti.com>
2019-01-01 17:09:51 -0800

    emacs-ffi testing resulted in a new approach, here named libjulia
    changes
    
    - libjulia.el generates bindings via emacs-ffi for the unbox functions + a few other helpful
      top-level functions.
    
    - julia-core.c replaced by simpler libjulia-wrapper.c which
      - calls dlopen on libjulia.so with RTDL_GLOBAL set (this means we no longer
      need to change emacs C sources!)
      - adds the GPL compliance def
      - calls jl_init (which we might be able to move to elisp)

--- Makefile
+++ Makefile
@@ -8,13 +8,10 @@ JL_FLAGS = $(shell $(JL_SHARE)/julia-config.jl --cflags --ldflags --ldlibs)
 all: lib
 
 lib: *.c
-	gcc $^ -shared -Wall $(JL_FLAGS) -lffi -lltdl -o julia-core.so
+	gcc $^ -shared -Wall $(JL_FLAGS) -lffi -lltdl -o libjulia-wrapper.so
 
 clean:
 	rm *.o *.so
 
-test: lib julia-tests.el Makefile
-	$(EMACS) --module-assertions -nw -Q -batch -L . -l ert -l julia-tests.el -f ert-run-tests-batch-and-exit
-
-test-ffi: lib julia-ffi-test.el Makefile
-	$(EMACS) --module-assertions -nw -Q -batch -L . -l ert -l julia-ffi-test.el -f ert-run-tests-batch-and-exit
+test: lib libjulia-test.el Makefile
+	$(EMACS) --module-assertions -nw -Q -batch -L . -l ert -l seq -l libjulia-test.el -f ert-run-tests-batch-and-exit
---
+++ libjulia-test.el
@@ -0,0 +1,45 @@
+(require 'ert)
+(require 'libjulia)
+
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Comparison-of-Numbers.html
+(defun float-equal (x y)
+  (let ((float-tol 1.0e-6))
+    (or (= x y)
+        (< (/ (abs (- x y))
+              (max (abs x) (abs y)))
+           float-tol))))
+
+(ert-deftest test-libjulia-primitive-julia-type-p ()
+  (should (libjulia-primitive-julia-type-p "Int64"))
+  (should (libjulia-primitive-julia-type-p "Float64"))
+  (should-not (libjulia-primitive-julia-type-p "String"))
+  (should-not (libjulia-primitive-julia-type-p "SomethingElse")))
+
+(ert-deftest test-libjulia-get-type ()
+  ;; Ints
+  (should (equal (libjulia-get-julia-type "3") "Int64"))
+  (should (equal (libjulia-get-julia-type "typemax(Int32)") "Int32"))
+  (should (equal (libjulia-get-julia-type "typemin(Int64)") "Int64"))
+  (should (equal (libjulia-get-julia-type "0x1") "UInt8"))
+  ;; negating an unsigned literal creates an unsigned 2s complement
+  (should (equal (libjulia-get-julia-type "-0x1000") "UInt16"))
+
+  ;; floats
+  (should (equal (libjulia-get-julia-type "3.14f0") "Float32"))
+  (should (equal (libjulia-get-julia-type "3.14") "Float64")))
+
+;; Implicitly tests unboxing
+(ert-deftest test-libjulia-eval ()
+  ;; primitives
+  (should (equal (libjulia-eval "3") 3))
+  (should (float-equal (libjulia-eval "3.14f0") 3.14))
+  (should (float-equal (libjulia-eval "3.14") 3.14))
+
+  ;; strings
+  (should (equal (libjulia-eval "\"Hallo\"") "Hallo")))
+
+;; (ert-deftest test-libjulia-null-ptr ()
+;;   (unwind-protect
+;;       (setq res (libjulia-eval "Julia sees this string unquoted, which returns a null pointer from jl_eval_string."))
+;;     (should nil)
+;;     (message (format "%s" res))))
